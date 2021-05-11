28bcbf10e0ed744f69dd9ae7d06a9474d65866b0
Raluca Ciulpan <raluuu26@gmail.com>
2021-05-12 02:24:06 +0300

    created functionality for signup and login buttons (with placeholder function for checkCredentials)

--- src/main/java/org/fis/pc/controllers/LoginController.java
+++ src/main/java/org/fis/pc/controllers/LoginController.java
@@ -1,4 +1,45 @@
 package org.fis.pc.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
+    private Parent root;
+    private Stage stage;
+    private Scene scene;
+    private FXMLLoader loader;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    public boolean checkUserCredentials(String usernameField, String passwordField){
+        return true;
+    }
+
+    @FXML
+    void handleLoginAction() {
+        if(checkUserCredentials(usernameField.getText(),passwordField.getText())){
+            System.out.println("Log in successful!");
+        }
+    }
+
+    @FXML
+    void handleSignupAction() throws IOException {
+        loader = new FXMLLoader(getClass().getClassLoader().getResource("signup.fxml"));
+        root = loader.load();
+        scene = new Scene(root, 1280,720);
+        stage = (Stage) usernameField.getScene().getWindow();
+        stage.setScene(scene);
+    }
+
 }
