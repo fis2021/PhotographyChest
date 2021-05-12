205c1ef9b6b6195e6b70256ada1f6453ac378e93
Raluca Ciulpan <raluuu26@gmail.com>
2021-05-13 02:05:22 +0300

    added some more placeholder methods

--- src/main/java/org/fis/pc/services/UserService.java
+++ src/main/java/org/fis/pc/services/UserService.java
@@ -1,7 +1,21 @@
 package org.fis.pc.services;
 
+import java.util.Objects;
+
 public class UserService {
+
+    public static String encodePassword(String salt, String password)
+    {
+        return password;
+    }
+
+
     public static boolean checkLoginCredentials(String username, String password){
-        return true;
+        if(Objects.equals(password,encodePassword(username,password))) {
+            System.out.println("login successful");
+            return true;
+        }
+        return false;
     }
 }
+
