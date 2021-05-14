cad0d15a8ec0a34f311398ee42d6688be8e2a28d
Raluca Ciulpan <raluuu26@gmail.com>
2021-05-14 22:14:11 +0300

    made homepage button functional /PC-53

--- src/main/java/org/fis/pc/controllers/ViewPortfolioController.java
+++ src/main/java/org/fis/pc/controllers/ViewPortfolioController.java
@@ -1,27 +1,48 @@
 package org.fis.pc.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+import org.fis.pc.model.Post;
+
+import java.io.IOException;
 
 public class ViewPortfolioController {
 
-    @FXML
-    private TableColumn<?, ?> ownerColumn;
+    private FXMLLoader loader;
+    private Parent root;
+    private Scene scene;
+    private Stage stage;
 
     @FXML
-    private TableColumn<?, ?> nameColumn;
+    private TableView<Post> table;
+    @FXML
+    private TableColumn<Post, String> ownerColumn;
 
     @FXML
-    private TableColumn<?, ?> priceColumn;
+    private TableColumn<Post, String> nameColumn;
 
     @FXML
-    private TableColumn<?, ?> categoryColumn;
+    private TableColumn<Post, String> priceColumn;
 
     @FXML
-    private TableColumn<?, ?> dateColumn;
+    private TableColumn<Post, String> categoryColumn;
 
     @FXML
-    void handleHomepageAction() {
+    private TableColumn<Post, String> dateColumn;
+
+
+    @FXML
+    void handleHomepageAction() throws IOException {
+        loader = new FXMLLoader(getClass().getClassLoader().getResource("photographerHomepage.fxml"));
+        root = loader.load();
+        scene = new Scene(root, 1280,720);
+        stage = (Stage) table.getScene().getWindow();
+        stage.setScene(scene);
 
     }
 
