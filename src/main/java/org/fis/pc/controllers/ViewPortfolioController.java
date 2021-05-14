4cad38ee1f13831f08e36277cd5989eee2cf2bcb
Raluca Ciulpan <raluuu26@gmail.com>
2021-05-14 22:06:09 +0300

    created gui for portfolio page /PC-51

---
+++ src/main/java/org/fis/pc/controllers/ViewPortfolioController.java
@@ -0,0 +1,28 @@
+package org.fis.pc.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+
+public class ViewPortfolioController {
+
+    @FXML
+    private TableColumn<?, ?> ownerColumn;
+
+    @FXML
+    private TableColumn<?, ?> nameColumn;
+
+    @FXML
+    private TableColumn<?, ?> priceColumn;
+
+    @FXML
+    private TableColumn<?, ?> categoryColumn;
+
+    @FXML
+    private TableColumn<?, ?> dateColumn;
+
+    @FXML
+    void handleHomepageAction() {
+
+    }
+
+}
